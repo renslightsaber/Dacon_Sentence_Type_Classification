1401762fb4c60af3d201a92918b72e0edb9abc1c
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-03-10 13:23:50 +0900

    Update new_trainer.py

--- new_trainer.py
+++ new_trainer.py
@@ -4,8 +4,13 @@ import gc
 import random
 import time
 
+import copy
+from copy import deepcopy
+
 import numpy as np
 import pandas as pd
+
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
